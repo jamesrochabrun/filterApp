6750f0d27db7fa1559888f074a6bdf0d47513b12
james <jamesrochabrun@gmail.com>
2016-08-08 22:38:56 -0700

    Initial Commit

---
+++ FilterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FilterApp
+//
+//  Created by James Rochabrun on 8/8/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FilterApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FilterApp
+//
+//  Created by James Rochabrun on 8/8/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
